944204d32978bee12fc48934d9df7c8c5fc2ef26
Daisuke Nakagome <torinokaraage@gmail.com>
2023-02-25 15:21:01 +0900

    `Getting Started`

--- libs/client.js
+++ libs/client.js
@@ -2,6 +2,6 @@ import { createClient } from 'microcms-js-sdk';
 
 export const client = createClient({
   serviceDomain: 'ebk44omjnb',  // service-domain は XXXX.microcms.io の XXXX 部分
-  apiKey: 'NwmfEqX9fONoxHuJIOciQ8H3VOS3qykNEJ1N',
+  apiKey: process.env.API_KEY,
 });
 
